f34df8211ca91f36b8123f6da7a9b8faecd6efb2
AlexeyBaranau <63657746+AlexeyBaranau@users.noreply.github.com>
2021-04-14 17:14:08 +0300

    Fixes.

--- common/src/main/java/by/finalproject/exception/EntityNotFoundException.java
+++ common/src/main/java/by/finalproject/exception/EntityNotFoundException.java
@@ -2,6 +2,8 @@ package by.finalproject.exception;
 
 public class EntityNotFoundException extends RuntimeException {
 
+  private static final long serialVersionUID = -6201562974266615960L;
+
   public EntityNotFoundException() {
     super();
   }
